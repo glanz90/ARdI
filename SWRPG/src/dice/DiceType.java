3628f4d6267e0da8b5dbfcea4281dbf1d6ee1d9a
meisl <meisl@DESKTOP-D5869UC.local>
2016-11-13 15:46:51 +0100

    Dice and Symbols

---
+++ SWRPG/src/dice/DiceType.java
@@ -0,0 +1,16 @@
+package dice;
+
+public enum DiceType {
+	W006Dice,
+	W008Dice,
+	W012Dice,
+	W020Dice,
+	W100Dice,
+	ForceDice,
+	BoostDice,
+	AbilityDice,
+	ProficiencyDice,
+	SetbackDice,
+	DifficultyDice,
+	ChalengeDice
+}
